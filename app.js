929075fea0ec50393bd411a5f997af007d0a0d4b
Addarshkumar <addarsh.kumar@kalvium.community>
2023-12-10 16:21:46 +0530

    part 2

---
+++ app.js
@@ -0,0 +1,29 @@
+class ReactContainer extends React.Component{
+
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
+  }
+
+
+// or
+
+// function ReactContainer(){
+    
+//           return (
+//           <div>Hello! Welcome to Kalvium   
+//              <div>This is babel</div>
+//          </div> )
+
+// }
+
+
+const container = document.getElementById('react-container');
+ReactDOM.render(<ReactContainer/>,container);
+
+// or
+
+// ReactDOM.render(React.createElement(ReactContainer),container);
